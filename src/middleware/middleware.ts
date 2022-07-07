d5d8581b28f2ed49c7d30349e28b4ed1620341f0
Thiago Borges <thiagogithub@gmail.com>
2022-07-07 20:12:13 -0300

    consertando alguns erros de builder

--- src/middleware/middleware.ts
+++ src/middleware/middleware.ts
@@ -1,4 +1,4 @@
-import jwt from 'jsonwebtoken'
+const jwt = require('jsonwebtoken')
 const middlewareValidateJWT = (req, res, next) => {
   const bearer = req.headers.authorization
   if (!bearer) {
